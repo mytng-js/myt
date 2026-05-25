f798d0eec74979964578b715e8547204b66f3cfd
jxf <1637787429@qq.com>
2026-05-25 17:53:47 +0800

    chore(config): add oxlint, oxfmt, tsconfig

---
+++ scripts/verify-commit.mjs
@@ -0,0 +1,26 @@
+/**
+ * @source https://github.com/vuejs/core
+ */
+import path from 'node:path'
+import fs from 'node:fs'
+import chalk from 'chalk'
+
+const msgPath = path.resolve('.git/COMMIT_EDITMSG')
+const msg = fs.readFileSync(msgPath, 'utf-8').trim()
+
+const commitRE =
+  /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?: .{1,50}/
+
+if (!commitRE.test(msg)) {
+  console.log()
+  console.error(
+    `  ${chalk.white(chalk.bgRed(' ERROR '))} ${chalk.red(`invalid commit message format.`)}\n\n` +
+      chalk.red(
+        `  Proper commit message format is required for automated changelog generation. Examples:\n\n`,
+      ) +
+      `    ${chalk.green(`feat(compiler): add 'comments' option`)}\n` +
+      `    ${chalk.green(`fix(bind:value): handle events on blur (close #28)`)}\n\n` +
+      chalk.red(`  See .github/commit-convention.md for more details.\n`),
+  )
+  process.exit(1)
+}
